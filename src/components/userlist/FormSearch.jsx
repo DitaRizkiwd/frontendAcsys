15e7a0263c899394b3829ab1ea38ea207b3f1ae6
unknown <rdita201@gmail.com>
2024-05-14 21:51:16 +0700

    add form ad user

--- src/components/userlist/FormSearch.jsx
+++ src/components/userlist/FormSearch.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FiPlus, FiSearch } from 'react-icons/fi'
+import FormAddUser from './FormAddUser'
+import Link from 'next/link'
 
 const FormSearch = () => {
   return (
     <div className="flex justify-between items-center">
-    <button className="bg-blue-500 flex items-center gap-2 py-2 px-3 rounded-xl text-white">
+      <Link href="/main/administrator/userlist/adduser">
+      <button className="bg-blue-500 flex items-center gap-2 py-2 px-3 rounded-xl text-white">
       <FiPlus size={20} />
       <p >Add</p>
-    </button>
+    </button>   
+      </Link>
       <form className="flex items-center border border-gray-400 rounded-xl">
         <input
           type="text"
